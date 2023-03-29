779c0e86d27f6da1be7164d4c2ed971e920e792e
jinwooverse@gmail.com <91906275+jiahn728@users.noreply.github.com>
2023-03-28 21:08:51 -0700

    updated data detail

--- course_scrape/class_scarper.py
+++ course_scrape/class_scarper.py
@@ -4,10 +4,9 @@ import requests
 import numpy as np 
 from bs4 import BeautifulSoup
 
-
 course_win_url = 'https://www.washington.edu/students/timeschd/WIN2023/cse.html'
 
-data_dictionary = ['SLN', 'Course_Name', 'Section', 'Limit', 'Class day', 'Class Time', 'Prereq', 'Required Section Amount']
+data_dictionary = ['SLN', 'Course_Name', 'Section', 'Required Section Amount', 'Limit', 'Class day', 'Class Time']
 #Prereq is not includes in this version
 
 #The function intakes the course schedule from UW and extracts (returns) all the text information 
@@ -84,8 +83,8 @@ def clean_course_info(web_extract):
         
         SectionID.append(each_info[1])        
         
-        #Handle #If ClassType -> note Lecture, if quiz -> section ID 
-        print(each_info[2])
+        #Handle Class types
+        #If ClassType -> note Lecture, if quiz -> section ID 
         if each_info[2] == "QZ":
             ClassType.append(each_info[2])
         elif each_info[2] == "LB":
@@ -108,18 +107,12 @@ def clean_course_info(web_extract):
         for i in range(each_info_length-1): 
             if '/' in each_info[i][-1]:
                 Limit.append(each_info[i+1])
-    
-    target_set = [SLN, SectionID, ClassType, Class_day, Class_time, Limit]
-    print(ClassType)
-    print(len(SLN), len(SectionID), len(ClassType), len(Class_day), len(Limit))
+    target_set = [SLN, SectionID, Limit, Class_day, Class_time]
+    #print(len(SLN), len(SectionID), len(ClassType), len(Class_day), len(Limit))
     return(target_set)
 
-#Collect the target information (descriped in the above data dictionary)
-filter_data = clean_course_info(web_extract[0])
-
-print('start')
-
 #Run to add customized columns for NFT mint 
+#Returns dataset(lists) with updated course name and required amount of section 
 def customize_for_contract(course_pair, filter_data): 
     #Add [Course Name, Required Section Amount']
     section_Amount = [] 
@@ -131,15 +124,6 @@ def customize_for_contract(course_pair, filter_data):
     course_tickers = course_pair[0]
     course_names = course_pair[1]
 
-    lecture_cnt = 0 
-    for element in all_sectionID: 
-        if "Lecture" in element: 
-            lecture_cnt += 1 
- 
-    total_class_cnt = len(course_pair[0]) 
-    print(total_class_cnt)
- 
-    #### 
     class_cnt = len(all_sectionID)
 
     pair_cnt = 0 
@@ -148,30 +132,59 @@ def customize_for_contract(course_pair, filter_data):
         #Fill same course untill the next class shows up 
         if all_sectionID[i] == "A": 
             #Update course name, ticker 
-            pair_cnt += 1 
-            update_course_ticker.append(course_tickers[(pair_cnt - 1)]) #Since the first value is always a Lecture
+            pair_cnt += 1
+            update_course_ticker.append(course_tickers[(pair_cnt - 1)].replace("\xa0","" ).replace(" ", ""))   
+            #Since the first value is always a Lecture
             update_course_name.append(course_names[(pair_cnt - 1)])
         else: 
-            update_course_ticker.append(course_tickers[(pair_cnt - 1)])
+            update_course_ticker.append(course_tickers[(pair_cnt - 1)].replace("\xa0","" ).replace(" ", ""))
             update_course_name.append(course_names[(pair_cnt - 1)])
-
 
         #Fill same course untill the next class shows up 
         #If one digit and the following is not, then does not have a section 
-
-        if len(all_sectionID[i]) == 1 and i+1 < class_cnt and len(all_sectionID[i+1]) == 2:
-             section_Amount.append('1')
+        if len(all_sectionID[i]) == 1 and i+1 < class_cnt and len(all_sectionID[i+1]) == 1:
+             section_Amount.append('0')
         #if the class has the lab, quiz, or seminar, indicate the number 
         else:
-            section_Amount.append('0')
-    
-    print(len(update_course_name), len(update_course_ticker), len(all_sectionID), len(section_Amount))
+            section_Amount.append('1')
+
+        #filter ">"
+        all_sln = filter_data[0]
+        filter_data[0] = [each_sln.replace('>', '') for each_sln in all_sln]
+
+        #Filter "E"
+        all_limit = filter_data[2]
+        filter_data[2] = [each_limit.replace('E', '') for each_limit in all_limit]
+        
+
+    #print(len(update_course_name), len(update_course_ticker), len(all_sectionID), len(section_Amount))
     combine_dataset = filter_data + [update_course_ticker] + [update_course_name] + [section_Amount]
     return combine_dataset
+         
+#The function intakes the filtered file, and convert into data frame, with a target order 
+def frame_data(final_data):
+    print(data_dictionary) 
 
+    df = pd.DataFrame({'SLN': final_data[0], 
+                       'Course Name': final_data[6],
+                       'Course Ticker': final_data[5], 
+                       'Section': final_data[1],
+                       'Required Section Amount': final_data[7], 
+                       'Limit': final_data[2], 
+                       'Class Day': final_data[3], 
+                       'Class Time': final_data[4]
+                       })
+    return df
+
+#Run functions 
+print('start')
+#Collect the target information (descriped in the above data dictionary)
+filter_data = clean_course_info(web_extract[0])
+#Customize the filtered data to fit the NFT contract 
 final_data = customize_for_contract(web_extract[1], filter_data)
+#Convert to csv file 
+final_dataframe = frame_data(final_data)
+
 print('end')
 
-with open('CSE_extract.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(final_data)
+final_dataframe.to_csv('output.csv', index=False)
